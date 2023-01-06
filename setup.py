06c6aa3aae746af3b5851f7e75d5c1043de15855
jrudar <37217429+jrudar@users.noreply.github.com>
2023-01-05 22:14:59 -0500

    Update setup.py
    Updated required packages.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="TreeOrdination",
-                 version="1.0.1",
+                 version="1.0.2",
                  author="Josip Rudar, G. Brian Golding, Stefan C. Kremer, Mehrdad Hajibabaei",
                  author_email="rudarj@uoguelph.ca",
                  description="Decision Tree Ensembles Utilizing Multivariate Splits Are Effective at Investigating Beta-Diversity in Medically Relevant 16S Amplicon Sequencing Data",
@@ -13,7 +13,9 @@ setup(name="TreeOrdination",
                  install_requires = ["numpy == 1.23.3",
                                      "pandas == 1.5.0",
                                      "scikit-learn == 1.1.2",
+                                     "scikit-bio == 0.5.7",
                                      "umap-learn == 0.5.3",
+                                     "deicode == 0.2.4"
                                      ],
                  classifiers=["Programming Language :: Python :: 3.10+",
                               "License :: MIT License",
