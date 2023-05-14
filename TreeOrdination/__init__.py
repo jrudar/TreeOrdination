63e67d8d7cbd614521b4b70425a9bce7f35207bb
Josip Rudar <Josip Rudar>
2023-05-14 02:06:19 -0400

    Reformatting using black

--- TreeOrdination/__init__.py
+++ TreeOrdination/__init__.py
@@ -5,4 +5,4 @@ This module imports TreeOrdination
 from .TreeOrdination import TreeOrdination
 from .transformers_treeord import NoTransform, CLRClosureTransformer, NoResample
 
-__all__ = ['TreeOrdination', "NoTransform", "CLRClosureTransformer", "NoResample"]
+__all__ = ["TreeOrdination", "NoTransform", "CLRClosureTransformer", "NoResample"]
